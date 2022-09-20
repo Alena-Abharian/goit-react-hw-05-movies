0931f5566754da1d56f8322daf5d42fb4c5fb40b
Alena-Abharian <alenar2531@gmail.com>
2022-09-21 00:30:47 +0300

    btn go back

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
@@ -8,10 +8,10 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter basename='/goit-react-hw-05-movies/'>
       <ThemeProvider theme={theme}>
-      <App />
+        <App />
       </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
